45692a775c2c9a3e6bea73c816e7e5244a815a49
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-10-08 14:00:33 -0600

    Using signals demo

--- first-angular-app/src/app/user/user.component.ts
+++ first-angular-app/src/app/user/user.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
 
+  imagePath = computed(() => {
+    return 'assets/users/' + this.selectedUser().avatar;
+  });
+
+  // get imagePath() {
+  //   return 'assets/users/' + this.selectedUser.avatar;
+  // }
+
+  onSelectUser() {
+    this.selectedUser.set(
+      DUMMY_USERS[Math.floor(Math.random() * DUMMY_USERS.length)]
+    );
+  }
 }
