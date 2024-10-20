3bc4b2220c9121730151415b93d1abbde0f6d85f
Lyubonir Georgiev <lyubo12georgiev@gmail.com>
2024-10-20 16:54:25 -0600

    transforming values with pipes

---
+++ pipes-angular/src/app/temperature.pipe.ts
@@ -0,0 +1,46 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temp',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(
+    value: string | number | null,
+    inputType: 'cel' | 'fah',
+    outputType?: 'cel' | 'fah'
+  ) {
+    if (!value) {
+      return value;
+    }
+
+    let val: number;
+
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+    let tempSymbol: string;
+
+    if (inputType === 'cel' && outputType === 'fah') {
+      outputTemp = val * (9 / 5) + 32;
+    } else if (inputType === 'fah' && outputType === 'cel') {
+      outputTemp = (val - 32) * (5 / 9);
+    } else {
+      outputTemp = val;
+    }
+
+    let tempType: string;
+
+    if (outputType) {
+      tempSymbol = outputType === 'cel' ? 'C' : 'F';
+    } else {
+      tempSymbol = inputType === 'cel' ? 'C' : 'F';
+    }
+
+    return `${outputTemp.toFixed(2)} °${tempSymbol}`;
+  }
+}
